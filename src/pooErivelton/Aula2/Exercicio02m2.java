4503b47dada55625f4fd9a07d624971b05fd898a
joaogustavok <joaogustavokmiecik@gmail.com>
2021-08-05 19:14:16 -0300

    Erivelton POO

---
+++ src/pooErivelton/Aula2/Exercicio02m2.java
@@ -0,0 +1,18 @@
+package pooErivelton.Aula2;
+
+import java.util.Scanner;
+
+public class Exercicio02m2 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Informe os graus fah");
+        double fah = ler.nextDouble();
+        double resultado = calcular(fah);
+        System.out.println(resultado);
+    }
+
+    public static double calcular(double a){
+        double result = (a - 32) / 1.8;
+        return result;
+    }
+}
